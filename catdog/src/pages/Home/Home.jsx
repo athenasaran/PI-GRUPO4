873a091018362a56833255ef194b3a3c947ab19a
AndrePX789 <andrepx_oliveira@hotmail.com>
2020-11-30 19:36:57 -0300

    upgrade Home

--- catdog/src/pages/Home/Home.jsx
+++ catdog/src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -9,6 +8,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import cat from "../../assets/img/maxresdefault.jpg"
+import dog from "../../assets/img/v3imagesbinb9bb06b04d755656942fb0ac75991dbc-kyjy7jq4mw9wx5drwt2_t1880.jpg"
 import Footer from "../../components/Footer/Footer"
 import "./Home.css"
 
@@ -20,27 +20,38 @@ const Home = () => {
             <div className="home">
                 <Grid container spacing={3}>
                     <Grid item xs={6}>
-                            <Card className="cg-card">
+                            <Card className="c-card">
                                 <CardActionArea>
-                                    <CardMedia component="img" alt="Foto de Gatos" title="Foto de Gatos" image={cat} />
+                                    <CardMedia component="img" alt="Foto de Gato" title="Foto de Gato" image={cat} />
                                     <CardContent>
                                         <h1>Cats</h1>
-                                        <h2>Gatos</h2>
-                                        <p>O gato, também conhecido como gato caseiro, gato urbano ou gato doméstico, é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação.
+                                        <p>O gato é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação.
                                              Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.</p>
                                     </CardContent>
                                 </CardActionArea>
                                 <CardActions>
-                                    <Button size="small" color="primary">
-                                        Leia Mais
+                                    <Button className="cg-botao" size="small" color="primary">
+                                        <strong>Saiba Mais</strong>
                                     </Button>
                                 </CardActions>
                             </Card>
                     </Grid>
                     <Grid item xs={6}>
-                        <Paper className="cg-card">
-                            <h1>Dog</h1>
-                        </Paper>
+                    <Card className="d-card">
+                                <CardActionArea>
+                                    <CardMedia component="img" alt="Foto de Cachorro" title="Foto de Cachorro" image={dog} />
+                                    <CardContent>
+                                        <h1>Dogs</h1>
+                                        <p>O cão é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano. 
+                                            Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos.</p>
+                                    </CardContent>
+                                </CardActionArea>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        <strong className="cg-botao">Saiba Mais</strong>
+                                    </Button>
+                                </CardActions>
+                            </Card>
                     </Grid>
                 
                 </Grid>
