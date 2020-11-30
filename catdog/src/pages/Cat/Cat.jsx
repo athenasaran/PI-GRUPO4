49f29b068b20acc7fb72c383299de888dc6df36f
AndrePX789 <andrepx_oliveira@hotmail.com>
2020-11-30 16:59:01 -0300

    Adição da base de Cat e Dog

---
+++ catdog/src/pages/Cat/Cat.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import "./Cat.css";
+
+const Sobre = () => {
+    return (
+        <>
+
+            <Header />
+            <div className="Cat">
+
+                <h1>Cat</h1>
+
+
+            </div>
+
+            <Footer />
+        </>
+    );
+};
+
+export default Cat;
---
+++ catdog/src/pages/Dog/Dog.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
+import "./Dog.css";
+
+const Sobre = () => {
+    return (
+        <>
+
+            <Header />
+            <div className="Dog">
+
+                <h1>Dog</h1>
+
+
+            </div>
+
+            <Footer />
+        </>
+    );
+};
+
+export default Dog;
