1725fada399f605b1fa2b1a65231db58a0d53460
AndrePX789 <andrepx_oliveira@hotmail.com>
2020-12-01 23:47:28 -0300

    Base do Sobre

--- catdog/src/pages/Sobre/Sobre.jsx
+++ catdog/src/pages/Sobre/Sobre.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 import Header from '../../components/Header/Header';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import logo from '../..//assets/img/catdog.png';
 import Footer from '../../components/Footer/Footer';
 import "./Sobre.css";
 
@@ -9,8 +15,90 @@ const Sobre = () => {
 
             <Header />
             <div className="sobre">
-
-                <h1>Sobre</h1>
+                <Grid container spacing={3}>
+                            <Grid item xs={6}>
+                                    <Card className="e-txt-card">
+                                        <CardActionArea>
+                                        
+                                            <CardContent>
+                                                
+                                                <h1>Text 1</h1>
+                                                
+                                            </CardContent>
+                                        </CardActionArea>
+                                        
+                                    </Card>
+                            </Grid>
+                            <Grid item xs={6}>
+                            <Card className="d-txt-card">
+                                        <CardActionArea>
+                                            
+                                            <CardContent>
+                                                <h1>Text 2</h1>
+                                                
+                                            </CardContent>
+                                        </CardActionArea>
+                                        
+                                    </Card>
+                            </Grid>
+                        
+                    </Grid>
+                    <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                                <Card className="e-txt-card">
+                                    <CardActionArea>
+                                    
+                                        <CardContent>
+                                            
+                                            <h1>Text 3</h1>
+                                            
+                                        </CardContent>
+                                    </CardActionArea>
+                                    
+                                </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card className="d-txt-card">
+                                    <CardActionArea>
+                                        
+                                        <CardContent>
+                                            <h1>Text 4</h1>
+                                            
+                                        </CardContent>
+                                    </CardActionArea>
+                                    
+                                </Card>
+                        </Grid>
+                    
+                </Grid>
+                <Grid container spacing={3}>
+                        <Grid item xs={6}>
+                                <Card className="e-txt-card">
+                                    <CardActionArea>
+                                    
+                                        <CardContent>
+                                            <h1>Text 5</h1>
+                                            
+                                        </CardContent>
+                                    </CardActionArea>
+                                    
+                                </Card>
+                        </Grid>
+                        <Grid item xs={6}>
+                        <Card className="d-txt-card">
+                                    <CardActionArea>
+                                        
+                                        <CardContent>
+                                        <CardMedia component="img" alt="Foto da Logo" title="Foto da Logo" image={logo} />
+                                            
+                                        </CardContent>
+                                    </CardActionArea>
+                                    
+                                </Card>
+                        </Grid>
+                    
+                </Grid>
+                
 
 
             </div>
