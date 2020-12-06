67a4933d2a659ca3862347fbb0007cff5a15bd1b
Fillipe Madureira <39290405+fillipelgm@users.noreply.github.com>
2020-12-06 19:33:14 -0300

    Adicao dos botoes de expansao nos cards

--- catdog/src/pages/Sobre/Sobre.jsx
+++ catdog/src/pages/Sobre/Sobre.jsx
@@ -9,10 +9,35 @@ import logo from '../..//assets/img/catdog.png';
 import Footer from '../../components/Footer/Footer';
 import "./Sobre.css";
 
+import { CardActions, CardHeader, IconButton, Collapse} from "@material-ui/core";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+const useStyles = makeStyles((theme) => ({
+    expand: {
+      transform: 'rotate(0deg)',
+      marginLeft: 'auto',
+      transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+      }),
+    },
+    expandOpen: {
+      transform: 'rotate(180deg)',
+    },
+  }));
+
 const Sobre = () => {
     
     function tSobre() {
         document.title = "Cat & Dog - Sobre"
+    };
+
+    const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
     };
 
     return (
@@ -24,23 +49,47 @@ const Sobre = () => {
                     <Grid item xs={6}>
                         <Card className="e-txt-card">
                             <CardActionArea>
+                                <CardHeader title="Quem Somos"/>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton 
+                                    className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>                            
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
                                 <CardContent>
-                                    <h2>Quem Somos</h2>
-                                    <p>Bem-vindo(a) ao <em>Cat & Dog</em>! Somos um site criado por amantes dos  bichinhos peludos mais fofos e populares do mundo.</p> 
+                                    <p>Bem-vindo(a) ao <em>Cat & Dog</em>! Somos um site criado por amantes dos  bichinhos peludos mais fofos e populares do mundo.</p>
                                     <p>A ideia para este site surgiu da necessidade de escolher um bom tema para o projeto final em um curso de programação. Tínhamos várias opções na mesa, mas nem todos concordávamos. Pesquisamos mais um pouco e chegamos em... cães e gatos!! Vai dizer que você nunca terminou aquela pesquisa séria pro trabalho da faculdade olhando fotos e vídeos de bichinhos às 2h da manhã?! Quem nunca passou por isso que atire a primeira pedra!</p>
                                     <p>Nosso intuito é compartilhar conhecimento (e fotos) sobre os <em>pets</em> e tentar acabar com a discussão sobre qual bichinho é melhor.</p>
                                 </CardContent>
-                            </CardActionArea>
+                            </Collapse>
                         </Card>
                     </Grid>
                     <Grid item xs={6}>
                         <Card className="d-txt-card">
                             <CardActionArea>
+                                <CardHeader title="Nosso Time"/>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton 
+                                    className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>                            
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
                                 <CardContent>
-                                    <h2>Nosso Time</h2>
                                     <p>Atualmente, somos formados por um time de desenvolvedores <em>web</em> que compartilham a paixão e o entusiasmo por animais.</p>
                                 </CardContent>
-                            </CardActionArea>
+                            </Collapse>
                         </Card>
                     </Grid>
                 </Grid>
@@ -49,39 +98,70 @@ const Sobre = () => {
                     <Grid item xs={6}>
                         <Card className="e-txt-card">
                             <CardActionArea>
+                                <CardHeader title="Junte-se a Nós!"/>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton 
+                                    className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>                            
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
                                 <CardContent>
-                                    <h2>Junte-se A Nós!</h2>
                                     <p>Queremos nos expandir e construir uma comunidade online de pessoas interessadas em <em>pets</em>, facilitando a interação entre elas e a troca de conhecimento. Você é entusiasta de cães e/ou gatos? Já trabalhou com isso? Sabe desenvolver aplicações? Tem alguma ideia incrível e quer se juntar ao time? Escreva-nos e nos conte mais. Ficaremos felizes com o seu interesse de se juntar ao time!</p>
                                 </CardContent>
-                            </CardActionArea>
+                            </Collapse>
                         </Card>
                     </Grid>
                     <Grid item xs={6}>
                         <Card className="d-txt-card">
                             <CardActionArea>
+                                <CardHeader title="Doe Hoje"/>
+                            </CardActionArea>
+                            <CardActions>
+                                <IconButton 
+                                    className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>                            
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
                                 <CardContent>
-                                    <h2>Doe Hoje</h2>
                                     <p>Quer ajudar animais de rua, mas não sabe como? Nós arrecadamos doações e ajudamos organizações sem fins lucrativos parceiras que já fazem essa boa ação!</p>
                                 </CardContent>
-                            </CardActionArea>   
+                            </Collapse>
                         </Card>
                     </Grid>
                 </Grid>
                 
                 <Grid container spacing={3}>
                     <Grid item xs={6}>
-                        <Card className="e-txt-card">
+                    <Card className="e-txt-card">
                             <CardActionArea>
-                                <CardContent>
-                                    <h2>Blablabla</h2>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci velit dolorem provident ipsam labore deserunt nihil, nemo similique tempora excepturi a id architecto optio error nobis ab aut in facere.Minus magnam vero, aliquam ipsa, quidem, error id doloremque reiciendis cupiditate quam eveniet mollitia harum? Recusandae eaque quos, saepe dignissimos, sequi esse magni vitae enim ipsum totam a, ab numquam!</p>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At, consequuntur. Iste sapiente culpa, a maiores illo consectetur ex! Explicabo, quam. Delectus odio quae recusandae unde. Quidem sit a voluptates cupiditate.
-                                    Necessitatibus accusantium architecto deserunt quia non doloribus accusamus expedita consequatur aspernatur autem sed beatae ratione eligendi earum repellat exercitationem inventore, totam eaque dignissimos maiores, tenetur harum! Voluptatibus quisquam vero ullam.
-                                    Ut aperiam odit sit temporibus dicta cumque quo. Quaerat natus libero explicabo accusantium, eum dignissimos. Odit ducimus possimus deleniti et expedita, hic exercitationem libero magnam minus architecto enim ipsum. At.
-                                    Ipsum consequuntur laudantium eligendi, qui error ducimus omnis hic eveniet cumque id modi nam deserunt doloremque, dolorem autem sed repellat eaque repellendus eius sint. Non, rem omnis. Minus, eaque mollitia?
-                                    Similique natus, quibusdam obcaecati et debitis vel iusto exercitationem odio aperiam! In sit facere necessitatibus molestias sequi dolorum. Placeat tempora recusandae modi nam quas non aspernatur cum iusto repellendus consequuntur?</p>
-                                </CardContent>
+                                <CardHeader title="Blá blá blá"/>
                             </CardActionArea>
+                            <CardActions>
+                                <IconButton 
+                                    className={clsx(classes.expand, {[classes.expandOpen]: expanded,})}
+                                    onClick={handleExpandClick}
+                                    aria-expanded={expanded}
+                                    aria-label="show more"
+                                >
+                                    <ExpandMoreIcon />
+                                </IconButton>                            
+                            </CardActions>
+                            <Collapse in={expanded} timeout="auto" unmountOnExit>
+                                <CardContent>
+                                    <p>Algo a mais para inserir? Fiquei sem ideias haha</p>
+                                </CardContent>
+                            </Collapse>
                         </Card>
                     </Grid>
                     <Grid item xs={6}>
@@ -94,10 +174,7 @@ const Sobre = () => {
                         </Card>
                     </Grid>
                 </Grid>
-                
-                
-
-
+               
             </div>
 
             <Footer />
