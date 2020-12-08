6dd2f31df47cab3b2b60c65c60fff0dae0294e58
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-12-07 21:17:07 -0300

    testando API

--- catdog/src/pages/Dog/Dog.jsx
+++ catdog/src/pages/Dog/Dog.jsx
@@ -1,24 +1,33 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import Header from '../../components/Header/Header';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
 import Footer from '../../components/Footer/Footer';
-import axios from 'axios'
 import "./Dog.css";
+import axios from 'axios';
 
 const Dog = () => {
-    const [dogs, setDogs] = useState([]);
+       const [data, setData] = useState([]);
 
 
-        async function loadDogs() {
-            axios.get('https://random.dog/woof.json')
-            .then(response => {
-                setDogs(response.data)
-            })
-            loadDogs();
-        }
+  useEffect(() => {
+    async function FetchData() {
+    await axios({
+        method: 'GET',
+        url: 'https://dog.ceo/api/breeds/image/random',
+    }).then((res) => {
+        setData(res.data)
+    });
+    }
+        FetchData();
+
+  }, []);
+
+
+
+
     function tDog() {
         document.title = "Cat & Dog - Dog"
     };
@@ -44,22 +53,12 @@ const Dog = () => {
 
                         </Card>
                     </Grid>
-                {
-                  dogs.map(dog => (
-                      <Grid item xs={6}>
-                          <Card className="dog-api-card">
-                            <CardActionArea>
-
-                                <CardContent>
-                                 <img src={dog.url} alt="Dog" />
-                                </CardContent>
-                            </CardActionArea>
-
-                        </Card>
-                          </Grid>
-                  ))
-                  }
                 </Grid>
+
+
+            </div>
+            <div className="dog-api">
+
 
 
             </div>
@@ -67,6 +66,5 @@ const Dog = () => {
             <Footer />
         </div>
     );
-
 }
 export default Dog;
