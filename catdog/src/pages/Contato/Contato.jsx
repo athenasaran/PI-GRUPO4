9ec4e441988af3fbe92cbae99ed49fd8a4be095c
AndrePX789 <andrepx_oliveira@hotmail.com>
2020-12-08 22:32:30 -0300

    Validação de Formulário

--- catdog/src/pages/Contato/Contato.jsx
+++ catdog/src/pages/Contato/Contato.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+
 import "./Contato.css"
 
 // import FormControl from '@material-ui/core/FormControl';
@@ -14,62 +15,112 @@ const Contato = () => {
     
     function tContato() {
         document.title = "Cat & Dog - Contato"
+        // document.getElementById('ResContato').style.display = "none"
     };
     
+    function validador() {
+        let nome = document.getElementById('nome-completo');
+        let email = document.getElementById('email');
+        let texto = document.getElementById('contato-texto');
+        
+        if (nome.value.length > 4){
+            var rNome = true;
+        };
+
+        if (email.value.length > 8){
+            var rEmail = true;
+        };
+
+        if (texto.value.length > 9){
+            var rTexto = true;
+        };
+       
+        if (rNome && rEmail && rTexto){
+            
+            document.getElementById('erros').innerHTML= ""
+            
+            document.querySelector('#titulo p').style.display = 'none'
+            document.getElementById('form-de-contato').innerHTML= "<h1>Obrigado, " + nome.value + "</h1><p>Entraremos em Contato!!</p>"
+                                                                    
+            
+            
+        }else{
+            document.getElementById('erros').innerHTML= ""
+
+            if(! rNome){
+                document.getElementById('erros').innerHTML+= "<ul><li>Campo <strong>Nome</Strong> deve ter no mínimo 5 letras"
+            }
+            if(! rEmail){
+                document.getElementById('erros').innerHTML+= "<ul><li>Campo <strong>Email</Strong> inválido"
+            }
+            if(! rTexto){
+                document.getElementById('erros').innerHTML+= "<ul><li>Campo <strong>Texto</Strong> deve ter no mínimo 10 letras"
+            }
+        };
+
+
+    }
+
     
     return (
         <div onLoad={tContato}>
 
             <Header />
+                
+                        <div className="contato">
+                            <div id="titulo">
+                                <h1>Contato</h1>
+                                <p>Use esse formulário para falar conosco!</p>
+                            </div>
+                            <div id="erros">
 
-            <div className="contato">
-                <div id="titulo">
-                    <h1>Contato</h1>
-                    <p>Use esse formulário para falar conosco!</p>
-                </div>
-                <div id="form-de-contato">
-                    <TextField
-                    id="nome-completo"
-                    label="Nome completo"
-                    style={{ margin:12}}
-                    placeholder="Insira seu nome"
-                    helperText="Com sobrenome!"
-                    fullWidth
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    />
-                    <TextField
-                    id="email"
-                    label="Email"
-                    style={{ margin: 12}}
-                    placeholder="exemplo@catdog.com"
-                    fullWidth
-                    color="secondary"
-                    margin="normal"
-                    InputLabelProps={{
-                        shrink: true,
-                    }}
-                    />
-                    <TextField
-                    style={{ margin: 12 }}
-                    id="contato-texto"
-                    label="Fale conosco!"
-                    multiline
-                    fullWidth
-                    rows={4}
-                    variant="outlined"
-                    />
-                    {/* nome
-                    email
-                    sugestão */}
-                    <Button variant="contained" color="primary"
-                    style={{marginLeft: 10}}>
-  Enviar
-</Button>
-                </div>
-            </div>
+                            </div>
+                            <div id="form-de-contato">
+                                <TextField
+                                id="nome-completo"
+                                label="Nome completo"
+                                style={{ margin:12}}
+                                placeholder="Insira seu nome"
+                                helperText="Com sobrenome!"
+                                fullWidth
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                    
+                                }}
+                                />
+                                <TextField
+                                id="email"
+                                label="Email"
+                                style={{ margin: 12}}
+                                placeholder="exemplo@catdog.com"
+                                fullWidth
+                                color="secondary"
+                                margin="normal"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                />
+                                <TextField
+                                style={{ margin: 12 }}
+                                id="contato-texto"
+                                label="Fale conosco!"
+                                multiline
+                                fullWidth
+                                rows={4}
+                                variant="outlined"
+                                />
+                            
+                                <Button variant="contained" color="primary"
+                                style={{marginLeft: 10}} onClick={validador}>
+                                Enviar
+                                </Button>
+                                <div className='ResContato'></div>
+                                
+                        </div>
+                    </div>
+                    
+            
             <Footer />
         </div>
     );
