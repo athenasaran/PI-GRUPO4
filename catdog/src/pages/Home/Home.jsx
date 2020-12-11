299c2f2abce566d3671973ee99cf7d00e36f61c7
kim <kimkakeya@gmail.com>
2020-12-10 21:10:32 -0300

    adição de responsividade na home

--- catdog/src/pages/Home/Home.jsx
+++ catdog/src/pages/Home/Home.jsx
@@ -49,43 +49,41 @@ const Home = () => {
 
             </AutoplaySlider>
             <div className="home">
-                <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                        <Card className="c-card">
-                            <CardActionArea>
-                                <CardMedia component="img" alt="Foto de Gato" title="Foto de Gato" image={cat_image} />
-                                <CardContent>
-                                    <h1>Cats</h1>
-                                    <p>O gato é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação.
-                                             Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.</p>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button className="cg-botao" size="small" >
-                                    <Link className="link" to="/cat">Saiba Mais</Link>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Card className="d-card">
-                            <CardActionArea>
-                                <CardMedia component="img" alt="Foto de Cachorro" title="Foto de Cachorro" image={dog_image} />
-                                <CardContent>
-                                    <h1>Dogs</h1>
-                                    <p>O cão é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano.
-                                            Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos.</p>
-                                </CardContent>
-                            </CardActionArea>
-                            <CardActions>
-                                <Button className="cg-botao" size="small" >
-                                    <Link className="link" to="/dog">Saiba Mais</Link>
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
+                <div className = "caixa">
 
-                </Grid>
+                    <Card className="c-card" onClick={() => window.location = "/cat"}>
+                        <CardActionArea>
+                            <CardMedia component="img" alt="Foto de Gato" title="Foto de Gato" image={cat_image} />
+                            <CardContent>
+                                <h1>Cats</h1>
+                                <p>O gato é um mamífero carnívoro da família dos felídeos, muito popular como animal de estimação.
+                                            Ocupando o topo da cadeia alimentar, é predador natural de diversos animais, como roedores, pássaros, lagartixas e alguns insetos.</p>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button className="cg-botao" size="small" >
+                                <Link className="link" to="/cat">Saiba Mais</Link>
+                            </Button>
+                        </CardActions>
+                    </Card>
+
+                    <Card className="d-card" onClick={() => window.location = "/dog"}>
+                        <CardActionArea>
+                            <CardMedia component="img" alt="Foto de Cachorro" title="Foto de Cachorro" image={dog_image} />
+                            <CardContent>
+                                <h1>Dogs</h1>
+                                <p>O cão é um mamífero carnívoro da família dos canídeos, subespécie do lobo, e talvez o mais antigo animal domesticado pelo ser humano.
+                                        Teorias postulam que surgiu do lobo cinzento no continente asiático há mais de 100 000 anos.</p>
+                            </CardContent>
+                        </CardActionArea>
+                        <CardActions>
+                            <Button className="cg-botao" size="small" >
+                                <Link className="link" to="/dog">Saiba Mais</Link>
+                            </Button>
+                        </CardActions>
+                    </Card>
+                </div>
+
             </div>
 
             <Footer />
